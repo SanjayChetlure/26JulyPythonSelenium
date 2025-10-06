a13b4ae5462e88541443e81ba369e1677c3092a8
SanjayChetlure <sanjay.chetlure@gmail.com>
2025-10-06 08:41:06 +0530

    6th Oct 2025

---
+++ Listbox/Ex6_getAllSelectedOptionsFromListbox.py
@@ -0,0 +1,19 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+
+driver=webdriver.Chrome()
+driver.get("file:///D:/26th%20July%202025/Selenium/Html%20files/Listbox.html")
+
+UN=driver.find_element(By.XPATH,"//select[@id='1234']")
+s=Select(UN)
+
+allSOptionsAddress=s.options     #[1stOptionWebElement, 2ndOptionWebElement,3rdOptionWebElement]
+
+for eachOptioAddress in allSOptionsAddress:
+    print(eachOptioAddress.text)
+
+
+
+time.sleep(10)
