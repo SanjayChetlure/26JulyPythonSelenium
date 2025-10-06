b4ed832021c1a33d814dec9a01003fa00f55098c
SanjayChetlure <sanjay.chetlure@gmail.com>
2025-10-06 08:41:23 +0530

    6th - Oct 2025

---
+++ Listbox/Ex4.1_getFirstSelectedOption_FRom_MultiSelectableListbox.py
@@ -0,0 +1,19 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+
+driver=webdriver.Chrome()
+driver.get("file:///D:/26th%20July%202025/Selenium/Html%20files/Listbox.html")
+
+UN=driver.find_element(By.XPATH,"//select[@id='1234']")
+s=Select(UN)
+
+s.select_by_index(0)
+s.select_by_index(1)
+
+print(s.first_selected_option.text)
+
+
+
+time.sleep(10)
