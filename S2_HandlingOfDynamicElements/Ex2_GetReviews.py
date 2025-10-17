37c25aeff5c383081e9e5c4fea235ea4fda2f928
SanjayChetlure <sanjay.chetlure@gmail.com>
2025-10-17 08:36:46 +0530

    17th - Oct 2025

---
+++ S2_HandlingOfDynamicElements/Ex2_GetReviews.py
@@ -0,0 +1,15 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver=webdriver.Chrome()
+driver.maximize_window()
+driver.get("https://www.flipkart.com/")
+time.sleep(2)
+driver.find_element(By.XPATH,"//input[@class='Pke_EE']").send_keys("redmi 12 5g")
+time.sleep(2)
+driver.find_element(By.XPATH,"//button[@class='_2iLD__']").click()
+time.sleep(5)
+
+textReview=driver.find_element(By.XPATH,"((//div[@class='tUxRFH'])[1]//span)[9]").text
+print(textReview)
